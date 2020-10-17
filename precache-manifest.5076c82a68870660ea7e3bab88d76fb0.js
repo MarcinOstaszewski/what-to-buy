bf2dc725c74730f0ae395fa77d093dab2ab284f8
MarcinOstaszewski <marcinostaszewski@gmail.com>
2020-10-18 01:45:37 +0200

    Updates

---
+++ precache-manifest.5076c82a68870660ea7e3bab88d76fb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f4ea86c3f91d81373a7f1549219b992",
+    "url": "/what-to-buy/index.html"
+  },
+  {
+    "revision": "e54fedc4b4dee4a8ecb3",
+    "url": "/what-to-buy/static/css/main.4f8a6d77.chunk.css"
+  },
+  {
+    "revision": "3556e04045ccc5c4ddad",
+    "url": "/what-to-buy/static/js/2.df62b7cc.chunk.js"
+  },
+  {
+    "revision": "5843ec24999d9bc4d5892cbe88e32df5",
+    "url": "/what-to-buy/static/js/2.df62b7cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e54fedc4b4dee4a8ecb3",
+    "url": "/what-to-buy/static/js/main.4dc65968.chunk.js"
+  },
+  {
+    "revision": "ba6ba043e647791421c3",
+    "url": "/what-to-buy/static/js/runtime-main.771fb3f9.js"
+  }
+]);
